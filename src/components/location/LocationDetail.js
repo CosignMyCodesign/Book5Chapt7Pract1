d431f053d22a8c19d2e6686c6729a4238d07fdf6
CosignMyCodesign <dillonwilliamsail@gmail.com>
2018-11-20 16:03:39 -0600

    fixed a minor styling bug

--- src/components/location/LocationDetail.js
+++ src/components/location/LocationDetail.js
@@ -13,7 +13,7 @@ export default class LocationDetail extends Component {
         const location = this.props.locations.find(a => a.id === parseInt(this.props.match.params.locationId)) || {}
 
         return (
-          <section className="location">
+          <section className="locations">
             <div key={location.id} className="card">
               <div className="card-body">
                 <h4 className="card-title">
